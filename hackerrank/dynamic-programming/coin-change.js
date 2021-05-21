c9dc39dc525c07d9b3584d040e2b90c7eb77bbad
Ayub-Begimkulov <begimkulov963@gmail.com>
2021-05-21 09:12:24 +0300

    coin change solution

---
+++ hackerrank/dynamic-programming/coin-change.js
@@ -0,0 +1,24 @@
+// https://www.hackerrank.com/challenges/coin-change/problem
+
+function getWays(amount, coins, index = 0, cache = new Map()) {
+  const cacheIdx = `${amount}_${index}`;
+  const cachedValue = cache.get(cacheIdx);
+  if (typeof cachedValue === "number") return cachedValue;
+  let result = 0;
+  for (let i = index, l = coins.length; i < l; i++) {
+    const diff = amount - coins[i];
+    if (diff === 0) {
+      result += 1;
+    } else if (diff > 0) {
+      // passing `i` to `getWays` recursively
+      // so it doesn't use prev coins to generate result
+      // since we already covered all possible options with
+      // them previously
+      result += getWays(diff, coins, i, cache);
+    }
+  }
+  cache.set(cacheIdx, result);
+  return result;
+}
+
+console.log(getWays(3, [2, 1, 3]));
